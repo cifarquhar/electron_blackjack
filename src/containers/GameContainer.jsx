50929253b92030afd3ca06bb502826f5975c798d
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2024-08-06 12:11:48 +0100

    Add deck generation logic

--- src/containers/GameContainer.jsx
+++ src/containers/GameContainer.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const GameContainer = () => {
 
+    const [deck, setDeck] = useState([]);
+
+    const generateDeck = () => {
+        const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+        const ranks = ["2", "3", "4", "5", "6", "7",  "8", "9", "10", "J", "Q", "K", "A"];
+
+        const cards = [];
+
+        suits.forEach(suit => {
+            ranks.forEach(rank => {
+                cards.push({suit, rank});
+            });
+        });
+
+        console.log(cards);
+        
+        setDeck(cards);
+    }
+
+    useEffect(generateDeck, []);
+
     return(
         <main>
-            <p>content goes here</p>
+            <p>stuff goes here</p>
         </main>
     )
 
